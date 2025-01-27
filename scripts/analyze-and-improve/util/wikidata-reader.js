05e47938319b5c405ffc24504ee3adb4ae10e84b
Cristan Meijer <cristan.meijer@gmail.com>
2025-01-27 09:10:03 +0100

    Whenever there are 2 wikidata pages for the same un/locode, use the oldest one

--- scripts/analyze-and-improve/util/wikidata-reader.js
+++ scripts/analyze-and-improve/util/wikidata-reader.js
@@ -15,10 +15,14 @@ export function readWikidata() {
         }
 
         if (wikiData[entry.unlocode]) {
-            // When an entry is there twice, keep the first one
-            wikiData[entry.unlocode].alternatives.push({
-                wikiDataEntry
-            })
+            // Whenever there are multiple items with the same UN/LOCODE, the one with the lowest number usually is best, since it was created first
+            // For example: London (Q84) vs Port of London (Q1545354)
+            const existingId = Number(wikiData[entry.unlocode].sourceUrl.split("Q").pop())
+            const thisId = Number(wikiDataEntry.sourceUrl.split("Q").pop())
+            if (thisId < existingId) {
+                wikiData[entry.unlocode] = wikiDataEntry
+            }
+
             return
         }
 
