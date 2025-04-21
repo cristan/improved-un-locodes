bc7b899040db6be78ddf6f9eaf6982c706aaf2e9
Cristan Meijer <cristan.meijer@gmail.com>
2025-04-21 15:59:47 +0200

    Update comment

--- scripts/analyze-and-improve/internal-validate-region-codes.js
+++ scripts/analyze-and-improve/internal-validate-region-codes.js
@@ -24,4 +24,4 @@ async function validateRegionCodes() {
 }
 
 validateRegionCodes()
-// I now have to do IDPNT onwards for indonesia, PGGUG onward for Papua New Guinea, IE and IL done, IN and JP, MY ignored
+// I now have to do IN, JP, MY
